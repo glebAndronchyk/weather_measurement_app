d20cb3a5f4141a65f4c1bba6414d2425a049a9e6
Gleb Andronchyk <gleb.andronchyK@gmail.com>
2025-05-10 13:10:55 +0300

    initialized map

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Map from "react-map-gl/mapbox";
+
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(import.meta.env);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        mapStyle="mapbox://styles/mapbox/standard"
+        style={{ width: 1200, height: 1200 }}
+        mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+      ></Map>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
